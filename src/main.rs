ab5463eefc190b522941d36dd57b8b74e7376d95
Malshan Bandara <malshanbandara82@gmail.com>
2025-06-08 22:50:33 +0530

    vc 1.0.1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+// Copyright (c) 2025 MALSHAN BANDARA [https://github.com/malshanb]
+// SPDX-License-Identifier: MIT
+
 use clap::{ArgGroup, Parser};
 use image::{DynamicImage, GenericImageView, RgbaImage};
 use std::fs;
